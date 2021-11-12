4219dc481339202e0a189d4ab85d12b59fe5c585
Gartenschlaeger <mail@kaisnet.de>
2021-11-12 08:34:22 +0100

    initial version

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <p>A new react app was just born :)</p>
+            </div>
+        );
+    }
+}
+
+export { App };
---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    // enty points
+    entry: path.resolve('src', 'index.jsx'),
+
+    // the output directory for the compiled and bundled files
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    // defines loaded for different content types
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+
+    // removes the need to add file extensions for import statements
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    // plugins
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+            filename: 'index.html'
+        })
+    ]
+};
